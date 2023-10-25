7f2ec7dddb5a8eee309126e9b1ac2233e1297011
KRATIK_BOHRA <kratikbohra5@gmail.com>
2023-10-26 00:15:42 +0530

    How to find directory and filename

--- Lecture-2/test.js
+++ Lecture-2/test.js
@@ -1,12 +1,18 @@
 // console.log(global);
-global.setTimeout(() => {
-  console.log("in the timeout");
-  clearInterval(int);
-}, 3000);
+// global.setTimeout(() => {
+//   console.log("in the timeout");
+//   clearInterval(int);
+// }, 3000);
 
-const int = setInterval(() => {
-  console.log("in the interval");
-}, 1000);
+// const int = setInterval(() => {
+//   console.log("in the interval");
+// }, 1000);
 
 // here the seInterval is goig untill you reach 3 second ni the settimeout where you clearInterval
 // and in the timeout is printed
+console.log(__dirname);
+console.log(__filename);
+// dirname gives directory
+// filename gives filename runnign
+
+// DOCUMENT.QURRYSELECTOR NOT WORKS IN SERVER SIDE

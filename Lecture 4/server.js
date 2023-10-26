0f95b2c5fe80a4ff27c753d6b2e073d98b042d8c
KRATIK_BOHRA <kratikbohra5@gmail.com>
2023-10-27 01:42:18 +0530

    leanrt nodejs ways to create routes through swithc case now will learn express js to do these things easily

--- Lecture 4/server.js
+++ Lecture 4/server.js
@@ -1,22 +1,61 @@
 const http = require("http");
 const fs = require("fs");
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.setHeader("Content-Type", "text/html");
-  const filePath = "C:/RepositoryProjects/Nodejs-Learn/index.html";
 
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      console.log(err);
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  console.log(req.url);
+
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  // res.write('<p>hello, ninjas</p>');
+  // res.write('<p>hello again, ninjas</p>');
+  // res.end();
+
+  // send html file
+  // fs.readFile('./views/index.html', (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //     res.end();
+  //   }
+  //   //res.write(data);
+  //   res.end(data);
+  // });
+
+  // routing
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-us":
+      res.statusCode = 301;
+      res.setHeader("Location", `/about`);
       res.end();
+      break;
+    default:
+      path += `404.html`;
+      res.statusCode = 404;
+  }
+
+  // send html
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.error(`Error reading file ${path}: ${err.message}`);
+      res.statusCode = 500; // Internal Server Error
+      res.end("Internal Server Error");
     } else {
-      // res.write(data);
-      // we can directly pass it into the end method
+      res.statusCode = 200;
       res.end(data);
     }
   });
 });
-
+// localhost is the default value for 2nd argument
 server.listen(3000, "localhost", () => {
-  console.log("listening to the port 3000");
+  console.log("listening for requests on port 3000");
 });
